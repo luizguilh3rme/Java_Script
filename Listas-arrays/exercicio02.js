4cbd07a10f7458d6fb0f4e14fb8113a401eee2f1
Luiz Guilherme Muniz barreto <166665419+luizguilh3rme@users.noreply.github.com>
2025-06-25 13:26:32 -0300

    att

---
+++ Listas-arrays/exercicio02.js
@@ -0,0 +1,12 @@
+
+//Imprimindo números pares de uma lista
+
+const listaNumeros = [1,2,3,4,5,6,7,8,9,10];
+
+for (let i = 0; i < listaNumeros.length; i++){
+    const numero = listaNumeros[i];
+
+    if(numero % 2 === 0){
+        console.log(numero);
+    }
+}
