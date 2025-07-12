1b877b4e14a39f6f32e38fdf6268bc498e0298bd
Luiz Guilherme Muniz barreto <166665419+luizguilh3rme@users.noreply.github.com>
2025-07-12 15:51:25 -0300

    Requisições Json

--- ProjetoJS/assets/js/main.js
+++ ProjetoJS/assets/js/main.js
@@ -1,4 +1,11 @@
-const btnTeste = document.getElementById("btnTeste");
-        btnTeste.addEventListener("click", function (){
-            console.log("clicou no botão");
-        })
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+//Fetch retorna uma Promise
+fetch(url)
+    //utilizando arrow function "=>", com encadeamento de THEN, o primeiro then vem do retorno da PROMISE,
+    //o segundo then, vem do retorno do primeiro e por ultimo o error
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))// Atraves desse Json de resposta conseguimos manipular o body
+    .catch((error) => console.error(error))
